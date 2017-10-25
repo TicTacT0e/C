0899fe4c4a1971e068eff62e57f02bcc1b1260c1
TicTacT0e <www.11sayros11@gmail.com>
2017-10-25 12:10:40 +0300

    update

--- discrete_math_lab/lab5_op12.c
+++ discrete_math_lab/lab5_op12.c
@@ -1,23 +1,23 @@
 #include "stdio.h"
 #include "malloc.h"
 
-/*Develop an algorithm for converting an incidence list 
- *into a list of pairs for undirected graph.*/
+/*Develop an algorithm for converting an incidence list
+*into a list of pairs for undirected graph.*/
 
 void incListInput(int**, int);
 void incListOutput(int**, int);
 
 #define BUF 50
-int incSize = 0; 
+int incSize = 0;
 
 int bufPairList[BUF][2];
 int pairList[BUF][2];
 int pair_count = 0;
-void addPair(int, int); 
-void pairListOutput(); 
+void addPair(int, int);
+void pairListOutput();
 
-void incList_to_pairList(int**, int); 
-void pairListClean(); 
+void incList_to_pairList(int**, int);
+void pairListClean();
 
 main()
 {
@@ -45,9 +45,9 @@ main()
 
 	incList_to_pairList(incList, tops);
 	pairListClean();
-	
+
 	printf("\n\tYOUR PAIR LIST\n");
-	pairListOutput(); 
+	pairListOutput();
 
 	for (int i = 0; i < tops; i++) {
 		free(incList[i]);
@@ -57,21 +57,28 @@ main()
 
 void pairListClean()
 {
+	int flag;
+	int a, b;
 	int list_ctr = 0;
+
 	for (int i = 0; i < BUF; i++)
 	{
-		if ((bufPairList[i][0] == bufPairList[i + 1][0]) && (bufPairList[i][1] == bufPairList[i + 1][1]))
-			continue;
-		else {
+		a = bufPairList[i][0]; b = bufPairList[i][1];
+		flag = 0;
+		for (int k = i + 1; k < BUF; k++)
+		{
+			if ((a == bufPairList[k][0] && b == bufPairList[k][1]) || (b == bufPairList[k][0] && a == bufPairList[k][1]))
+				flag++;
+		}
+		if (flag == 4 || flag == 2 || flag == 0)
+		{
 			pairList[list_ctr][0] = bufPairList[i][0];
 			pairList[list_ctr][1] = bufPairList[i][1];
-			list_ctr++; 
+			list_ctr++;
 		}
-
-		if (bufPairList[i][0] == 0 || bufPairList[i][1] == 0)
-			break;
 	}
 }
+
 
 void incList_to_pairList(int **incList, int tops)
 {
@@ -89,27 +96,27 @@ void addPair(int a, int b)
 {
 	bufPairList[pair_count][0] = a;
 	bufPairList[pair_count][1] = b;
-	pair_count++; 
+	pair_count++;
 }
 
 void pairListOutput()
 {
-	int flag = 1; 
+	int flag = 1;
 
 	for (int i = 0; i < BUF; i++)
 	{
 		for (int j = 0; j < 2; j++)
 		{
-			if (pairList[i][j] == 0 || pairList[i][j+1] == 0)
+			if (pairList[i][0] == 0 || pairList[i][1] == 0)
 			{
-				flag = 0; 
-				break; 
+				flag = 0;
+				break;
 			}
-			printf("%d ", pairList[i][j]); 
+			printf("%d ", pairList[i][j]);
 		}
-		printf("\n"); 
+		printf("\n");
 		if (flag == 0)
-			break; 
+			break;
 	}
 }
 
@@ -121,7 +128,7 @@ void incListInput(int **incList, int tops)
 	for (int i = 0; i < tops; i++)
 	{
 		count = 0;
-		printf("Incidence list for the %d", i + 1); 
+		printf("Incidence list for the %d", i + 1);
 		if ((i + 1) == 1)
 			printf("st ");
 		else if ((i + 1) == 2)
@@ -129,7 +136,7 @@ void incListInput(int **incList, int tops)
 		else if ((i + 1) == 3)
 			printf("rd ");
 		else printf("th ");
-		printf("vertex: "); 
+		printf("vertex: ");
 		for (int j = 0; (scanf_s("%d", &dig) != EOF); j++)
 		{
 			incList[i][j] = dig;
@@ -145,7 +152,7 @@ void incListOutput(int **incList, int tops)
 {
 	for (int i = 0; i < tops; i++)
 	{
-		printf("%d", i + 1); 
+		printf("%d", i + 1);
 		if ((i + 1) == 1)
 			printf("st ");
 		else if ((i + 1) == 2)
@@ -153,12 +160,13 @@ void incListOutput(int **incList, int tops)
 		else if ((i + 1) == 3)
 			printf("rd ");
 		else printf("th ");
-		printf("vertex: "); 
+		printf("vertex: ");
 		for (int j = 0; j < incSize; j++)
 		{
 			printf("%d ", incList[i][j]);
 		}
-		printf("\n"); 
+		printf("\n");
 	}
 	printf("\n");
 }
+
